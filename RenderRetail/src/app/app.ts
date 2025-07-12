0b4ad80b87b1d41df3fba35c7fb01c6d41b21ab1
Teo344 <gabriel2004mat@gmail.com>
2025-07-11 20:25:37 -0500

    estructura del Home

--- RenderRetail/src/app/app.ts
+++ RenderRetail/src/app/app.ts
@@ -3,8 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
 import { Footer } from './core/footer/footer';
 
+
+
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
--- RenderRetail/src/app/pages/home/home.ts
+++ RenderRetail/src/app/pages/home/home.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Home {
 
