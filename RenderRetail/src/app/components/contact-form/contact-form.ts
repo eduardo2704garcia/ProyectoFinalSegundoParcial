07d0de315c805f7af16cc38242371aef8ea29abe
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-07-13 09:59:14 -0500

    validaciones de formulario, página contact al 100% y página home al 100%

--- RenderRetail/src/app/components/contact-form/contact-form.ts
+++ RenderRetail/src/app/components/contact-form/contact-form.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.css'
 })
 export class ContactForm {
+  contactForm: FormGroup;
+  submitted = false;
+  showSuccess = false;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.contactForm.valid) {
+      this.showSuccess = true;
+      this.contactForm.reset();
+      this.submitted = false;
+
+      setTimeout(() => this.showSuccess = false, 4000);
+    }
+  }
 }
